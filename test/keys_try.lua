2e6d719040af2477cc1104dbb88ba6e4cb42f489
Masatoshi Teruya <mah0x211@gmail.com>
2014-11-20 16:05:37 +0900

    change: keys_try.lua has been updated.

--- test/keys_try.lua
+++ test/keys_try.lua
@@ -9,6 +9,7 @@ local val = {
         }
     }
 };
+local ttl = 2;
 local res;
 
 -- cleanup
@@ -25,3 +26,11 @@ ifNotEqual( inspect( val ), inspect( res.body.node.value ) );
 
 res = ifNil( cli:delete( key ) );
 ifNotEqual( res.status, 200 );
+
+
+res = ifNil( cli:set( key, val, ttl ) );
+ifNotEqual( res.status, 201 );
+sleep( ttl + 1 );
+res = ifNil( cli:get( key ) );
+ifNotEqual( res.status, 404 );
+
