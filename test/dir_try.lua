762a9d202a6be5e63444d9890c854f0db829972c
Masatoshi Teruya <mah0x211@gmail.com>
2014-11-21 19:34:47 +0900

    change: dir_try.lua has been updated.

--- test/dir_try.lua
+++ test/dir_try.lua
@@ -4,14 +4,14 @@ local cli = ifNil( Etcd.new() );
 local ttl = 2;
 local res;
 
-res = ifNil( cli:rmdir( '/', true ) );
+res = ifNil( cli:rmdir( '/path', true ) );
 res = ifNil( cli:mkdir( '/path/to/dir' ) );
 ifNotEqual( res.status, 201 );
 
 res = ifNil( cli:readdir( '/', true ) );
 ifNotEqual( res.status, 200 );
 
-res = ifNil( cli:rmdir( '/', true ) );
+res = ifNil( cli:rmdir( '/path', true ) );
 ifNotEqual( res.status, 200 );
 
 res = ifNil( cli:readdir( '/path/to/dir', true ) );
@@ -22,5 +22,4 @@ ifNotEqual( res.status, 201 );
 sleep( ttl + 1 );
 res = ifNil( cli:readdir( '/path', true ) );
 ifNotEqual( res.status, 404 );
-
 
