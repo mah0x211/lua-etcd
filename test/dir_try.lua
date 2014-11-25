0bbeea5f9a5cd7de505a5a66529867eb38910fbe
Masatoshi Teruya <mah0x211@gmail.com>
2014-11-25 17:05:47 +0900

    change: dir_try.lua has been updated.

--- test/dir_try.lua
+++ test/dir_try.lua
@@ -2,6 +2,7 @@ local Etcd = require('etcd.luasocket');
 
 local cli = ifNil( Etcd.new() );
 local recursive = true;
+local consistent = true;
 local res;
 
 -- cleanup
@@ -11,6 +12,9 @@ res = ifNil( cli:mkdir( '/path/to/dir' ) );
 ifNotEqual( res.status, 201 );
 
 res = ifNil( cli:readdir( '/', recursive ) );
+ifNotEqual( res.status, 200 );
+
+res = ifNil( cli:readdir( '/', recursive, consistent ) );
 ifNotEqual( res.status, 200 );
 
 res = ifNil( cli:rmdir( '/path', recursive ) );
@@ -24,6 +28,12 @@ res = ifNil( cli:mkdirnx( '/path/to/dir' ) );
 ifNotEqual( res.status, 201 );
 res = ifNil( cli:mkdirnx( '/path/to/dir' ) );
 ifNotEqual( res.status, 412 );
+
+
+res = ifNil( cli:set( '/path/to/key' ) );
+ifNotEqual( res.status, 201 );
+res = ifNil( cli:readdir( '/path/to/key' ) );
+ifNotEqual( res.status, 404 );
 
 -- cleanup
 ifNil( cli:rmdir( '/path', recursive ) );
