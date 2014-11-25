2ca27b0e348cf46129c34a618a727987c5de9c06
Masatoshi Teruya <mah0x211@gmail.com>
2014-11-25 16:09:56 +0900

    add: in_order_try.lua has been added.

---
+++ test/in_order_try.lua
@@ -0,0 +1,40 @@
+local Etcd = require('etcd.luasocket');
+
+local cli = ifNil( Etcd.new() );
+local key = '/path/to/dir'
+local val = {
+    structured = {
+        value = {
+            of = 'key'
+        }
+    }
+};
+local res;
+
+-- cleanup
+ifNil( cli:rmdir( '/path', true ) );
+
+res = ifNil( cli:set( key, val ) );
+ifNotEqual( res.status, 201 );
+
+res = ifNil( cli:push( key, val ) );
+ifNotEqual( res.status, 400 );
+
+res = ifNil( cli:rmdir( '/path', true ) );
+ifNotEqual( res.status, 200 );
+
+res = ifNil( cli:mkdir( key ) );
+ifNotEqual( res.status, 201 );
+
+res = ifNil( cli:push( key, val ) );
+ifNotEqual( res.status, 201 );
+
+res = ifNil( cli:push( key, val ) );
+ifNotEqual( res.status, 201 );
+
+res = ifNil( cli:readdir( key ) );
+ifNotEqual( res.status, 200 );
+ifNotEqual( #res.body.node.nodes, 2 );
+
+-- cleanup
+ifNil( cli:rmdir( '/path', true ) );
