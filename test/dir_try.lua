ba23cdbef072f833ba8ebc94ccfbe3bcaefe8c03
Masatoshi Teruya <mah0x211@gmail.com>
2014-11-25 11:51:18 +0900

    change: dir_try.lua has been updated.

--- test/dir_try.lua
+++ test/dir_try.lua
@@ -1,26 +1,22 @@
 local Etcd = require('etcd.luasocket');
 
 local cli = ifNil( Etcd.new() );
-local ttl = 2;
+local recursive = true;
 local res;
 
-res = ifNil( cli:rmdir( '/path', true ) );
+-- cleanup
+ifNil( cli:rmdir( '/path', recursive ) );
+
 res = ifNil( cli:mkdir( '/path/to/dir' ) );
 ifNotEqual( res.status, 201 );
 
-res = ifNil( cli:readdir( '/', true ) );
+res = ifNil( cli:readdir( '/', recursive ) );
 ifNotEqual( res.status, 200 );
 
-res = ifNil( cli:rmdir( '/path', true ) );
+res = ifNil( cli:rmdir( '/path', recursive ) );
 ifNotEqual( res.status, 200 );
 
-res = ifNil( cli:readdir( '/path/to/dir', true ) );
-ifNotEqual( res.status, 404 );
-
-res = ifNil( cli:mkdir( '/path', ttl ) );
-ifNotEqual( res.status, 201 );
-sleep( ttl + 1 );
-res = ifNil( cli:readdir( '/path', true ) );
+res = ifNil( cli:readdir( '/path/to/dir' ) );
 ifNotEqual( res.status, 404 );
 
 -- mkdir if not exists
@@ -30,4 +26,4 @@ res = ifNil( cli:mkdirnx( '/path/to/dir' ) );
 ifNotEqual( res.status, 412 );
 
 -- cleanup
-ifNil( cli:rmdir( '/path', true ) );
+ifNil( cli:rmdir( '/path', recursive ) );
